7cbcc05c519ba042a82e0212689b86d7c55496aa
David JULIEN <david.julien@gmail.com>
2023-09-07 08:54:42 +0200

    Initial commit

---
+++ lib/ex_ssl_options.ex
@@ -0,0 +1,18 @@
+defmodule ExSslOptions do
+  @moduledoc """
+  Documentation for `ExSslOptions`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSslOptions.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_ssl_options_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSslOptionsTest do
+  use ExUnit.Case
+  doctest ExSslOptions
+
+  test "greets the world" do
+    assert ExSslOptions.hello() == :world
+  end
+end
