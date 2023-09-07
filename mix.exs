1bb9dd8eb215a54c34a004d7c50f652195d4be46
David JULIEN <david.julien@wttj.co>
2023-09-07 09:26:51 +0200

    chore: add tools to check code quality
    credo, mix_audit, dialyxir

--- mix.exs
+++ mix.exs
@@ -5,12 +5,21 @@ defmodule ExSslOptions.MixProject do
     [
       app: :ex_ssl_options,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_options: [
         warnings_as_errors: true
       ],
-      deps: deps()
+      dialyzer: [
+        flags: [:unmatched_returns, :error_handling, :unknown, :extra_return],
+        list_unused_filters: true,
+        plt_add_apps: []
+      ],
+      preferred_cli_env: [
+        quality: :test
+      ],
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -24,7 +33,22 @@ defmodule ExSslOptions.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      # Dev deps
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases() do
+    [
+      quality: [
+        "format",
+        "credo --strict",
+        "deps.audit --format human",
+        "test --cover",
+        "dialyzer"
+      ]
     ]
   end
 end
