b643501c32b8aed868670217a971e00c9c7b6efe
David JULIEN <david.julien@wttj.co>
2024-11-07 16:19:41 +0100

    chore: new version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExSslOptions.MixProject do
   def project do
     [
       app: :ex_ssl_options,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       elixirc_options: [
